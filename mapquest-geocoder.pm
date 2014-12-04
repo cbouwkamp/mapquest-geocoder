8bbb29f431c300767f1d45db7d9642248a3c0119
Christopher A. Bouwkamp <cbouwkamp@cox.net>
2014-12-04 11:22:33 -0600

    Removed some of the module heading for now for easier testing

--- mapquest-geocoder.pm
+++ mapquest-geocoder.pm
@@ -1,32 +1,72 @@
+#!/usr/bin/perl 
 package mqgeocoder;
-use strict;
-use warnings;
-
-require Exporter;
-our @ISA = qw(Exporter);
-our @EXPORT_OK = qw();
 
 sub new{
-    my $type = shift;
-    my $class = ref $type || $type;
+    my $class = shift;
     my $self = {
-        CITY => undef,
-        STATE => undef,
-        ZIP => undef,
-        ADDRESS => undef,
-        KEY => undef    
+        _city => undef,
+        _state => undef,
+        _zip => undef,
+        _address => undef,
+        _key => undef    
     };
-    my $closure = sub {
-    my $field = shift;
-            @_ and $self->{$field} = shift;
-            $self->{$field};
-    };
-    bless $closure, $class;
-    $closure;
-    sub city { &{ $_[0] }("CITY", @_[1 .. $#_ ] ) }
-    sub state  { &{ $_[0] }("STATE",  @_[1 .. $#_ ] ) }
-    sub zip { &{ $_[0] }("ZIP", @_[1 .. $#_ ] ) }
-    sub address  { &{ $_[0] }("ADDRESS",  @_[1 .. $#_ ] ) }
-    sub key { &{ $_[0] }("KEY", @_[1 .. $#_ ] ) }
+    bless $self, $class;
+    return $self;
 }
 
+
+#Get and Set City Variables
+sub setCity{
+    my ( $self, $city ) = @_;
+    $self->{_city} = $city if defined($city);
+    return $self->{$city};
+}
+sub getCity {
+    my( $self ) = @_;
+    return $self->{_city};
+}
+
+#Get and Set State Variables
+sub setState{
+    my ( $self, $state ) = @_;
+    $self->{_state} = $state if defined($state);
+    return $self->{$state};
+}
+sub getState {
+    my( $self ) = @_;
+    return $self->{_state};
+}
+
+#Get and Set Zip Variables
+sub setZip{
+    my ( $self, $zip ) = @_;
+    $self->{_zip} = $zip if defined($zip);
+    return $self->{$zip};
+}
+sub getZip {
+    my( $self ) = @_;
+    return $self->{_zip};
+}
+
+#Get and Set Address Variables
+sub setAddress{
+    my ( $self, $address ) = @_;
+    $self->{_address} = $address if defined($address);
+    return $self->{$address};
+}
+sub getAddress {
+    my( $self ) = @_;
+    return $self->{_address};
+}
+
+#Get and Set Key Variables
+sub setKey{
+    my ( $self, $key ) = @_;
+    $self->{_key} = $key if defined($key);
+    return $self->{$key};
+}
+sub getKey {
+    my( $self ) = @_;
+    return $self->{_key};
+}
+1;
